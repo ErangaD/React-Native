57a589bb58e934aef22ec7db36d8120ed25e23a2
erangad <erangadulshan.14@cse.mrt.ac.lk>
2017-06-23 00:24:54 +0530

    created the navigation view

--- index.android.js
+++ index.android.js
@@ -8,46 +8,94 @@ import React, { Component } from 'react';
 import {
   AppRegistry,
   StyleSheet,
-  Text,
+  Navigator,
   View
 } from 'react-native';
-
+import Button from 'react-native-button';
+import MyScene from './Scenes/Control';
 export default class AwesomeProject extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.android.js
-        </Text>
-        <Text style={styles.instructions}>
-          Double tap R on your keyboard to reload,{'\n'}
-          Shake or press menu button for dev menu
-        </Text>
-      </View>
-    );
-  }
+    constructor(){
+        super();
+        this.state={
+            name:'',
+            selected:0
+        };
+        this.handleLearn=this.handleLearn.bind(this);
+        this.handleControl=this.handleControl.bind(this);
+    }
+    handleLearn(){
+
+
+    }
+    handleControl(){
+
+    }
+    render() {
+        let view=<View>
+        </View>;
+        switch (this.state.selected){
+            case 0:
+                view=<View>
+                    <Button
+                        style={styles.learningButton}
+                        styleDisabled={{color: 'red'}}
+                        onPress={() => this.handleLearn()}>
+                        Learning Mode
+                    </Button>
+                    <Button
+                        style={styles.controlButton}
+                        styleDisabled={{color: 'red'}}
+                        onPress={() => this.handleControl()}>
+                        Control Mode
+                    </Button>
+
+                </View>;
+                break;
+            case 1:
+                break;
+            case 2:
+                break;
+        }
+        return (
+
+            view
+
+        );
+    }
 }
 
 const styles = StyleSheet.create({
+    learningButton:{
+        fontSize: 40,
+        textAlign: 'center',
+        margin: 100,
+        marginTop:200
+    },
+    controlButton:{
+        fontSize: 40,
+        textAlign: 'center',
+        color: '#333333'
+    },
+    controlButton:{
+        //numbers from 1 to 10
+    },
   container: {
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    backgroundColor: '#F5FCFF'
   },
   welcome: {
     fontSize: 20,
     textAlign: 'center',
-    margin: 10,
+    margin: 100,
+    marginTop:250
   },
   instructions: {
     textAlign: 'center',
     color: '#333333',
-    marginBottom: 5,
-  },
+    marginBottom: 5
+  }
 });
 
 AppRegistry.registerComponent('AwesomeProject', () => AwesomeProject);
